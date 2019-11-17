95cb6295bd2c97e628e584923431653be8735163
Vishwa Vijay <im.vishwavijay@gmail.com>
2019-11-17 20:41:02 +0530

    added ssl parameters

---
+++ server/simple.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	network "github.com/govishwavijay/learning/libs"
+)
+
+func main() {
+	log.Println("Starting server________")
+	network.RedirectToHTTPS(8080, "vishwavijay.com")
+
+	server := http.Server{Addr: ":8080", Handler: nil}
+	server.HandleFunc("/", handleRootContext)
+
+	publicKey := "/home/vijay/public.cer"
+	privateKey := "/home/vijay/privatekey.pem"
+	if err := server.ListenAndServeTLS(publicKey, privateKey); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
+
+func handleRootContext(writer http.ResponseWriter, request *http.Request) {
+	log.Println("Request came from : ", network.ReadRemoteIP(request), request.RequestURI)
+	fmt.Fprintf(writer, "Hello there !! I am still working on this.")
+}
